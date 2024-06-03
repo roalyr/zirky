bd107b54113686a039896e22cfaf318044cb0d5a
Roman Yeremenko <roal.yr@gmail.com>
2024-06-04 02:15:25 +0300

    SHADER: Nebula outside shader. Star sprite tweak

--- Scripts/Autoload/autoload_Paths.gd
+++ Scripts/Autoload/autoload_Paths.gd
@@ -4,7 +4,7 @@ onready var viewport_container_3D = ViewportContainer3d
 onready var viewport_3D = viewport_container_3D.get_node("Viewport3D")
 onready var environment = viewport_3D.get_node("Environment")
 onready var global_space = viewport_3D.get_node("Global_space")
-onready var nebula_global = global_space.get_node("Nebula_global")
+#onready var nebula_global = global_space.get_node("Nebula_global")
 
 onready var local_space_system = viewport_3D.get_node("Local_space_system")
 onready var local_space_star = viewport_3D.get_node("Local_space_star")
